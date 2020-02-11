239cafaaf4c3a67bdfdeadcf4b76b20581e639b9
Dragana Večić <dragana1986.r@gmail.com>
2020-02-11 20:16:43 +0100

    Added new methodefor class car

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -15,62 +15,57 @@ public class Main {
         
         String message = "Ovo je poruka";
         
-        Car fiat = new Car();
-        fiat.model = "Fiat Punto";
-        fiat.buildYear = 2010;
-        fiat.color = "black";
-        fiat.consumption = 6;
-        fiat.maxSpeed = 180;
-        fiat.currentFuel = 20;
-        fiat.maxFuel = 45;
-        
-        System.out.println("Model: " + fiat.model);
-        System.out.println("Godina proizvodnje: " + fiat.buildYear);
-        System.out.println("Boja: " + fiat.color);
-        System.out.println("Potrosnja: " + fiat.consumption);
-        System.out.println("Maximalna brzina: " + fiat.maxSpeed);
-        System.out.println("Trenutno stanje goriva: " + fiat.currentFuel);
-        System.out.println("Maximalna potrosnja: " + fiat.maxFuel);
+        Car fiat = new Car("Fiat Punto", 2010, "black", 45);
+        fiat.showData();
+        //Car = ("Fiat Punto", 2010, "black", 6, 20, 45)
         
         
+        /*fiat.setModel("Fiat Punto");
+        fiat.setBuildYear(2010);
+        fiat.setColor("black");
+        fiat.setConsumption(6);
+        fiat.setCurrentFuel(20);
+        fiat.setMaxFuel(45);
+        fiat.setMaxSpeed(170);*/
         
-        Car ford = new Car();
-        ford.model = "Ford fiesta";
-        ford.buildYear = 2015;
-        ford.color = "white";
-        ford.consumption = 5;
-        ford.maxSpeed = 160;
-        ford.currentFuel = 30;
-        ford.maxFuel = 55;
+        
+        Car ford = new Car("Ford", 2015, "red", 40, 10, 5); 
+        ford.showData();
+        
+        /*ford.setModel("Ford fiesta");
+        ford.setBuildYear(2015);*/
         
         
         
-        System.out.println("Model: " + ford.model);
-        System.out.println("Godina proizvodnje: " + ford.buildYear);
-        System.out.println("Boja: " + ford.color);
-        System.out.println("Potrosnja: " + ford.consumption);
-        System.out.println("Maximalna brzina: " + ford.maxSpeed);
-        System.out.println("Trenutno stanje goriva: " + ford.currentFuel);
-        System.out.println("Maximalna potrosnja: " + ford.maxFuel);
+        //System.out.println("Model: " + ford.getModel());
         
-        Car peugeot = new Car();
-        peugeot.model = "Peugeot 3008";
-        peugeot.buildYear = 2017;
-        peugeot.color = "silver";
-        peugeot.consumption = 6;
-        peugeot.maxSpeed = 170;
-        peugeot.currentFuel = 35;
-        peugeot.maxFuel = 60;
+        Car lad = new Car("Lada Niva", 1980, "green", 50, 40, 12);
+        lada.showData();
         
         
         
-        System.out.println("Model: " + peugeot.model);
-        System.out.println("Godina proizvodnje: " + peugeot.buildYear);
-        System.out.println("Boja: " + peugeot.color);
-        System.out.println("Potrosnja: " + peugeot.consumption);
-        System.out.println("Maximalna brzina: " + peugeot.maxSpeed);
-        System.out.println("Trenutno stanje goriva: " + peugeot.currentFuel);
-        System.out.println("Maximalna potrosnja: " + peugeot.maxFuel);
+        
+        
+        
+       // Car peugeot = new Car();
+      //  peugeot.model = "Peugeot 3008";
+      //  peugeot.buildYear = 2017;
+       // peugeot.color = "silver";
+       // peugeot.consumption = 6;
+       // peugeot.maxSpeed = 170;
+       // peugeot.currentFuel = 35;
+       // peugeot.maxFuel = 60;
+        
+        
+        
+       // System.out.println("Model: " + peugeot.model);
+      //  System.out.println("Godina proizvodnje: " + peugeot.buildYear);
+       // System.out.println("Boja: " + peugeot.color);
+        //System.out.println("Potrosnja: " + peugeot.consumption);
+       // System.out.println("Maximalna brzina: " + peugeot.maxSpeed);
+       // System.out.println("Trenutno stanje goriva: " + peugeot.currentFuel);
+       // System.out.println("Maximalna potrosnja: " + peugeot.maxFuel);
+        
         
         
         
