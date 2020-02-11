cb28a96cc8a047a5cd7df4ac58967b8b3cff6c9e
Dragana Večić <dragana1986.r@gmail.com>
2020-02-11 17:47:17 +0100

    New car added.

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -37,9 +37,40 @@ public class Main {
         Car ford = new Car();
         ford.model = "Ford fiesta";
         ford.buildYear = 2015;
+        ford.color = "white";
+        ford.consumption = 5;
+        ford.maxSpeed = 160;
+        ford.currentFuel = 30;
+        ford.maxFuel = 55;
+        
+        
+        
         System.out.println("Model: " + ford.model);
+        System.out.println("Godina proizvodnje: " + ford.buildYear);
+        System.out.println("Boja: " + ford.color);
+        System.out.println("Potrosnja: " + ford.consumption);
+        System.out.println("Maximalna brzina: " + ford.maxSpeed);
+        System.out.println("Trenutno stanje goriva: " + ford.currentFuel);
+        System.out.println("Maximalna potrosnja: " + ford.maxFuel);
+        
+        Car peugeot = new Car();
+        peugeot.model = "Peugeot 3008";
+        peugeot.buildYear = 2017;
+        peugeot.color = "silver";
+        peugeot.consumption = 6;
+        peugeot.maxSpeed = 170;
+        peugeot.currentFuel = 35;
+        peugeot.maxFuel = 60;
         
         
+        
+        System.out.println("Model: " + peugeot.model);
+        System.out.println("Godina proizvodnje: " + peugeot.buildYear);
+        System.out.println("Boja: " + peugeot.color);
+        System.out.println("Potrosnja: " + peugeot.consumption);
+        System.out.println("Maximalna brzina: " + peugeot.maxSpeed);
+        System.out.println("Trenutno stanje goriva: " + peugeot.currentFuel);
+        System.out.println("Maximalna potrosnja: " + peugeot.maxFuel);
         
         
         
